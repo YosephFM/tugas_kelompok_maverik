0466739b383aab52d7e438a2a42140204b327964
YosephFM <yosephfelixmarchello_2327240065@mhs.mdp.ac.id>
2025-10-08 15:06:48 +0700

    Initial commit

---
+++ bin/tugas_kelompok_maverik.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_kelompok_maverik/tugas_kelompok_maverik.dart' as tugas_kelompok_maverik;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_kelompok_maverik.calculate()}!');
+}
---
+++ test/tugas_kelompok_maverik_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_kelompok_maverik/tugas_kelompok_maverik.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
